a273f37a32ef89b80994baba1b3932440d7ad51b
nhat51 <65001571+nhat51@users.noreply.github.com>
2022-01-13 10:10:59 +0700

    up main

---
+++ java-spring-tutorial/src/main/java/com/example/javaspringtutorial/JavaSpringTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringTutorialApplication.class, args);
+    }
+
+}
---
+++ java-spring-tutorial/src/test/java/com/example/javaspringtutorial/JavaSpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
